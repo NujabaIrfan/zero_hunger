12589695e8d55392569f9c80c3b0b8ec0ccf1738
Seniru <senirupasan@gmail.com>
2025-08-16 13:59:20 +0530

    chore: implement screen navigation

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CreateOrganization from './src/screens/CreateOrganization';
+import Home from './src/screens/Home';
+import Signup from './src/screens/Signup';
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: "home",
+  screens: {
+    home: Home,
+    createOrganization: CreateOrganization,
+    signup: Signup
+  },
+})
+
+const Navigation = createStaticNavigation(RootStack);
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigation />
   );
 }
 
---
+++ src/screens/Home.jsx
@@ -0,0 +1,15 @@
+import { useNavigation } from "@react-navigation/native";
+import { Button, Text, View } from "react-native";
+
+export default function Home({}) {
+
+  const navigator = useNavigation()
+
+  return (
+    <View>
+      <Text>Home page</Text>
+      <Button title="Create account"  onPress={() => navigator.navigate("signup")} />
+      <Button title="Create organization"  onPress={() => navigator.navigate("createOrganization")} />
+    </View>
+  )
+}
