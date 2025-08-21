8ce96ba24585900bd918975082e658228c1131dd
Nujaba Irfan <nujaba30@gmail.com>
2025-08-21 17:51:07 +0530

    remove api

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,14 @@ import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBUOXWRGsdA0XEYgNQK35W-C-JPyIynJ0c',
-  authDomain: 'foodrescue-d889c.firebaseapp.com',
-  projectId: 'foodrescue-d889c',
-  storageBucket: 'foodrescue-d889c.firebasestorage.app',
-  messagingSenderId: '539694017488',
-  appId: '1:539694017488:web:083217c6c45e82ec43a7d5',
-  measurementId: 'G-LBP7QDK60W',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+
 };
 
 // Initialize Firebase
